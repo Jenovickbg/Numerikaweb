eba386687edc82f50edfec10dc7053dc2f88b828
Jenovic Kabongo <jenovickbg5@gmail.com>
2026-09-23 22:38:49 +0100

    first commit11

--- js/redirect.js
+++ js/redirect.js
@@ -1,10 +1,10 @@
 /**
  * Redirection vers une ancre de la page vitrine.
- * Usage : <script src="js/redirect.js" data-to="accueil.html#offre"></script>
+ * Usage : <script src="js/redirect.js" data-to="index.html#offre"></script>
  */
 (function () {
   "use strict";
   var script = document.currentScript;
-  var target = (script && script.getAttribute("data-to")) || "accueil.html";
+  var target = (script && script.getAttribute("data-to")) || "index.html";
   window.location.replace(target);
 })();
